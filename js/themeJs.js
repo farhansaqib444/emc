83b1e1bf9b2e2d3abd3ea53a50a8e7f48340a2e4
bendevjb <bendev@jobesk.com>
2020-07-31 01:01:04 +0500

    Initial commit

---
+++ js/themeJs.js
@@ -0,0 +1,10 @@
+// show password icon
+
+$(".reveal").on('click',function() {
+    var $pwd = $(".pwd");
+    if ($pwd.attr('type') === 'password') {
+        $pwd.attr('type', 'text');
+    } else {
+        $pwd.attr('type', 'password');
+    }
+});
