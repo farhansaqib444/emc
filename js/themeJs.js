6d9e013dc4142b0fbe20d0abe8defcbf30ff4734
bendevjb <bendev@jobesk.com>
2020-08-07 18:37:21 +0500

    dashboard

--- js/themeJs.js
+++ js/themeJs.js
@@ -8,3 +8,62 @@ $(".reveal").on('click',function() {
         $pwd.attr('type', 'password');
     }
 });
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("fixed-top");
+    } else {
+        header.classList.remove("fixed-top");
+    }
+}
+
+
+
+$(document).ready(function(){
+    $(".owl-carousel.review-carousel").owlCarousel( {
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center:true,
+        dots: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".owl-carousel.client-logo").owlCarousel( {
+        dots: true,
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center:true,
+        responsive:{
+            0:{
+                items:2,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+});
+
